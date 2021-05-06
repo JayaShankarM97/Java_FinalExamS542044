2aafc4633b0a8762ace42fdd30e6d5b8a331819c
JayaShankar Mangina <77761176+JayaShankarM97@users.noreply.github.com>
2021-05-06 17:59:26 -0500

    Update AppleiPhone.java

--- Mangina_Spring2021FinalExam/src/EqualsandHashCode_Pb11/AppleiPhone.java
+++ Mangina_Spring2021FinalExam/src/EqualsandHashCode_Pb11/AppleiPhone.java
@@ -8,6 +8,7 @@ package EqualsandHashCode_Pb11;
 /**
  *
  * @author JayaShankar Mangina
+ * Declaring attributes
  */
 public class AppleiPhone {
     private int serialID;
@@ -15,10 +16,17 @@ public class AppleiPhone {
     private String display;
     private String processor;
 
+    /**
+     * Initiated an empty constructor
+     */
     public AppleiPhone() {
         
     }
 
+    /**
+     * Getter methods to get the values from the respective attributes
+     * @return 
+     */
     public int getSerialID() {
         return serialID;
     }
@@ -35,6 +43,10 @@ public class AppleiPhone {
         return processor;
     }
 
+    /**
+     * Setter methods to set the values for respective attributes
+     * @param serialID 
+     */
     public void setSerialID(int serialID) {
         this.serialID = serialID;
     }
@@ -51,6 +63,18 @@ public class AppleiPhone {
         this.processor = processor;
     }
 
+    /**
+     * We are overriding the default equals() method provided by the java
+     * in order to achieve desired output and application behavior
+     * If we don't override the equals() method , the java would look up to the,
+     * methods defined by the object class and executes them,
+     * which in return we get objects address.In the below method, we are
+     * evaluating the conditions through which we get to know whether the,
+     * the objects we created and the objects that Java has built within is,
+     * having the same contents and values.
+     * @param obj
+     * @return 
+     */
     @Override
     public boolean equals(Object obj) {
         if(obj == null){
@@ -67,6 +91,14 @@ public class AppleiPhone {
         return (this.getSerialID() == i.getSerialID());
     }
 
+    /**
+     * As we overridden the equals() method, we must override the hashCode(),
+     * method.The HashCode() method optimize performance when comparing objects.When
+     * both the objects aren't same in hashcode() we need not to use equals() method.This
+     * method will compare both the object references and the contents in both
+     * the objects and returns true if the objects passes all the conditions.
+     * @return 
+     */
     @Override
     public int hashCode() {
          final int NUMBER = 31;
